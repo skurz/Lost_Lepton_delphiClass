d15c956874b27813b0a469ffad95a968a8ff0052
Simon Kurz <Simon.Kurz@desy.de>
2015-11-11 14:27:32 +0100

    smaller changes

---
+++ MakeExpectation_Signal.C
@@ -0,0 +1,41 @@
+#include <TChain.h>
+#include <vector>
+#include "TProofServ.h"
+#include "TProof.h"
+#include "TLorentzVector.h"
+#include "ExpecMaker.h"
+using std::vector;
+
+//needed to write vector<TLorentzVector> to tree
+#ifdef __CINT__
+#pragma link C++ class std::vector<TLorentzVector>+;
+#endif
+
+void MakeExpectation_Signal()
+{
+  bool useTProof = false;
+
+  TProof *proof = NULL;
+  if(useTProof){
+    proof = TProof::Open("lite://", "workers=2");
+  }
+  
+  gSystem->Load("libPhysics.so");
+  gInterpreter->GenerateDictionary("vector<TLorentzVector>","TLorentzVector.h;vector");
+  
+  TChain *Effchain = new TChain("TreeMaker2/PreSelection");
+  Effchain->Add("/nfs/dust/cms/user/kurzsimo/LostLepton/signal_v3/*SMS-T1tttt_mGluino-1500_mLSP-100*.root");
+
+
+  if(useTProof){
+    Effchain->SetProof();
+  }
+
+  Effchain->Process("ExpecMaker.C++g", "Expectation_T1tttt_1500_100.root"); //specify output path/file here // folder already has to exist for the time beeing...
+  // No HT cut: Effchain->Process("ExpecMaker.C++g", "outputFile.root");
+  // HT cut: Effchain->Process("ExpecMaker.C++g", "outputFile.root, genHTcut");
+  // for Jacks Syntax (outputname based on input) use: Effchain->Process("ExpecMaker.C++g", "*, genHTcut");
+
+  delete proof;
+
+}
