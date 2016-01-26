0e207e4852ef94b920ea83aca06c9e51bdb28617
Simon Kurz <Simon.Kurz@desy.de>
2016-01-26 14:36:57 +0100

    minor fixes

---
+++ MakeExpectationLibraries.C
@@ -0,0 +1,22 @@
+#include <TChain.h>
+#include <vector>
+#include "TProofServ.h"
+#include "TProof.h"
+#include "TLorentzVector.h"
+#include "ExpecMaker.h"
+
+using std::vector;
+
+//needed to write vector<TLorentzVector> to tree
+#ifdef __CINT__
+#pragma link C++ class std::vector<TLorentzVector>+;
+#endif
+
+void MakeExpectationLibraries()
+{
+  
+  gSystem->Load("libPhysics.so");
+  gInterpreter->GenerateDictionary("vector<TLorentzVector>","TLorentzVector.h;vector");
+
+  gROOT->ProcessLine(".L ExpecMaker.C+");
+}
---
+++ MakePredictionLibraries.C
@@ -0,0 +1,24 @@
+#include <TChain.h>
+#include <vector>
+#include "TProofServ.h"
+#include "TProof.h"
+#include "TLorentzVector.h"
+#include "TROOT.h"
+#include "Prediction.h"
+
+
+using std::vector;
+
+//needed to write vector<TLorentzVector> to tree
+#ifdef __CINT__
+#pragma link C++ class std::vector<TLorentzVector>+;
+#endif
+
+
+void MakePredictionLibraries()
+{
+  gSystem->Load("libPhysics.so");
+  gInterpreter->GenerateDictionary("vector<TLorentzVector>","TLorentzVector.h;vector");
+
+  gROOT->ProcessLine(".L Prediction.C+");
+}
